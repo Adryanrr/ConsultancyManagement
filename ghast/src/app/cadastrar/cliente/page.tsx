d086543c16ba2939d27b61e4fb7dc44dcbefec52
MatheusJuK <matheusrjuca@gmail.com>
2024-11-04 21:19:07 -0300

    cadastro cliente responsivo e finalizado

--- ghast/src/app/cadastrar/cliente/page.tsx
+++ ghast/src/app/cadastrar/cliente/page.tsx
@@ -1,187 +1,290 @@
 'use client'
 
-import React from "react"
-import { ChevronRight, ChevronLeft } from "lucide-react"
-
-import Pagina from "@/components/template/Pagina"
+import { useState } from 'react'
+import { ChevronLeft } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-
-
+import Pagina from '@/components/template/Pagina'
+import Image from 'next/image'
 
 export default function Component() {
-
-  const [step, setStep] = React.useState(1)
-  const [formData, setFormData] = React.useState({
-    companyName: "",
-    cnpj: "",
-    corporateEmail: "",
-    sector: "",
-    representativeName: "",
-    cpf: "",
-    email: "",
-    phone: ""
+  const [step, setStep] = useState(1)
+  const [formData, setFormData] = useState({
+    companyName: '',
+    cnpj: '',
+    corporateEmail: '',
+    sector: '',
+    representativeName: '',
+    cpf: '',
+    email: '',
+    phone: ''
   })
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target
-    setFormData((prev) => ({ ...prev, [name]: value }))
+    const { id, value } = e.target
+    setFormData(prev => ({ ...prev, [id]: value }))
   }
 
   const handleSelectChange = (value: string) => {
-    setFormData((prev) => ({ ...prev, sector: value }))
+    setFormData(prev => ({ ...prev, sector: value }))
   }
+
+  const isStepOneComplete = () => {
+    return (
+      formData.companyName &&
+      formData.cnpj &&
+      formData.corporateEmail &&
+      formData.sector &&
+      formData.representativeName &&
+      formData.cpf &&
+      formData.email &&
+      formData.phone
+    )
+  }
+
   const handleNext = () => {
-    setStep((prev) => Math.min(prev + 1,2))
+    if (isStepOneComplete()) {
+      setStep(2)
+    }
   }
 
   const handleBack = () => {
-    setStep((prev) => Math.max(prev - 1,1))
-  }
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    console.log('Formulário enviado:',formData)
+    setStep(1)
   }
 
-  return (
-    <Pagina>
-    <Card className="w-full max-w-4xl mx-auto border-blue-500 border-2">
-      <CardHeader className="border-b">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-2">
-            <div className="w-6 h-6 rounded-full bg-purple-500 text-white flex items-center justify-center text-sm">
-              1
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    if (isStepOneComplete()) {
+      console.log('Form submitted:', formData)
+      // Here you would typically send the data to your backend
+    }
+  }
+
+  if (step === 2) {
+    return (
+      <Pagina>
+        <Card className="w-full max-w-4xl mx-10 border-blue-500 border-2">
+          <CardHeader className="border-b">
+            <div className="flex justify-between items-center">
+              <div className="flex items-center gap-2">
+                <div className="w-6 h-6 rounded-full bg-purple-500 text-white flex items-center justify-center text-sm">
+                  1
+                </div>
+                <CardTitle className="text-gray-600">Empresa</CardTitle>
+              </div>
+              <div className="flex items-center gap-2">
+                <div className="w-6 h-6 rounded-full bg-purple-500 text-white flex items-center justify-center text-sm">
+                  2
+                </div>
+                <span className="text-gray-600">Finalizar</span>
+              </div>
             </div>
-            <CardTitle className="text-gray-600">Empresa</CardTitle>
-          </div>
-          <div className="flex items-center gap-2 text-gray-400">
-            <div className="w-6 h-6 rounded-full bg-gray-200 flex items-center justify-center text-sm">
-              2
+          </CardHeader>
+          <CardContent className="p-6">
+            <div className="flex flex-col md:flex-row justify-between items-center gap-8">
+              <div className="w-full md:w-1/2 space-y-6">
+                <div className="space-y-2">
+                  <Label>Nome da Empresa</Label>
+                  <Input
+                    value={formData.companyName}
+                    className="border-gray-300 bg-gray-50"
+                    readOnly
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label>CNPJ</Label>
+                  <Input
+                    value={formData.cnpj}
+                    className="border-gray-300 bg-gray-50"
+                    readOnly
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label>Telefone</Label>
+                  <Input
+                    value={formData.phone}
+                    className="border-gray-300 bg-gray-50"
+                    readOnly
+                  />
+                </div>
+                <div className="space-y-2">
+                  <Label>Setor de Atuação</Label>
+                  <Input
+                    value={formData.sector}
+                    className="border-gray-300 bg-gray-50"
+                    readOnly
+                  />
+                </div>
+              </div>
+              <div className="w-full md:w-1/2 flex justify-center">
+                <Image src="/assets/confirmacaoCadastro.svg" alt='' width={338} height={338} className='hidden md:flex' />
+              </div>
             </div>
-            <span>Finalizar</span>
-          </div>
-        </div>
-      </CardHeader>
-      <CardContent className="p-6">
-        <form className="flex flex-row justify-center gap-10" onSubmit={handleSubmit}>
-          {/* lado esquerdo */}
-          <div className="space-y-6 flex-1">
-            <div className="space-y-2">
-              <Label htmlFor="company-name">Nome da Empresa</Label>
-              <Input
-                id="company-name"
-                placeholder="Nome completo da empresa"
-                className="border-gray-300"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="cnpj">CNPJ</Label>
-              <Input
-                id="cnpj"
-                placeholder="Digite o CNPJ"
-                className="border-gray-300"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="corporate-email">E-mail Corporativo</Label>
-              <Input
-                id="corporate-email"
-                type="email"
-                placeholder="Digite o e-mail corporativo"
-                className="border-gray-300"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="sector">Setor de Atuação</Label>
-              <Select value={formData.sector} onValueChange={handleSelectChange}>
-                <SelectTrigger id="sector">
-                  <SelectValue placeholder="Selecione o setor de atuação"/>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="admin">Administração</SelectItem>
-                  <SelectItem value="finance">Finanças</SelectItem>
-                  <SelectItem value="law">Direito</SelectItem>
-                  <SelectItem value="health">Saúde</SelectItem>
-                  <SelectItem value="Psychology">Psicologia</SelectItem>
-                  <SelectItem value="other">Outro</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-          {/* barra central */}
-          <div>
-            <div className="border-l border-gray-300 h-full"></div>
-          </div>
-          {/* lado direito */}
-          <div className="space-y-6 flex-1">
-            <div className="space-y-2">
-              <Label htmlFor="representative-name">Nome do Representante</Label>
-              <Input
-                id="representative-name"
-                placeholder="Nome completo do representante"
-                className="border-gray-300"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="cpf">CPF</Label>
-              <Input
-                id="cpf"
-                placeholder="Digite o CPF"
-                className="border-gray-300"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="email">E-mail</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="Digite o e-mail"
-                className="border-gray-300"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="phone">Telefone</Label>
-              <Input
-                id="phone"
-                placeholder="Digite o telefone"
-                className="border-gray-300"
-                onChange={handleInputChange}
-              />
-            </div>
-          </div>
-          
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
+          </CardContent>
+          <CardFooter className="flex justify-between">
             <Button
               type="button"
               variant="outline"
               onClick={handleBack}
-              disabled={step === 1}
-              className={step === 1 ? 'invisible' : ''}
             >
               <ChevronLeft className="w-4 h-4 mr-2" />
               Voltar
             </Button>
-            {step === 1 ? (
-              <Button type="button" onClick={handleNext}>
-                Continuar
-                <ChevronRight className="w-4 h-4 ml-2" />
-              </Button>
-            ) : (
-              <Button type="submit">Finalizar</Button>
-            )}
+            <Button
+              onClick={handleSubmit}
+              className="bg-purple-500 hover:bg-purple-600"
+            >
+              Finalizar
+            </Button>
           </CardFooter>
-    </Card>
+        </Card>
+      </Pagina>
+
+    )
+  }
+
+  return (
+    <Pagina>
+      <Card className="w-full max-w-4xl mx-5 border-blue-500 border-2">
+        <CardHeader className="border-b hidden md:flex">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-2">
+              <div className="w-6 h-6 rounded-full bg-purple-500 text-white flex items-center justify-center text-sm">
+                1
+              </div>
+              <CardTitle className="text-gray-600">Empresa</CardTitle>
+            </div>
+            <div className="flex items-center gap-2 text-gray-400">
+              <div className="w-6 h-6 rounded-full bg-gray-200 flex items-center justify-center text-sm">
+                2
+              </div>
+              <span>Finalizar</span>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent className="p-6 h-full">
+          <form className="flex flex-col md:flex-row justify-center gap-10" onSubmit={handleSubmit}>
+            {/* lado esquerdo */}
+            <div className="space-y-6 flex-1">
+              <div className="space-y-2">
+                <Label htmlFor="companyName" className='responsive-label' >Nome da Empresa</Label>
+                <Input
+                  id="companyName"
+                  placeholder="Nome completo da empresa"
+                  className="border-gray-300"
+                  value={formData.companyName}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="cnpj" className='responsive-label'>CNPJ</Label>
+                <Input
+                  id="cnpj"
+                  placeholder="Digite o CNPJ"
+                  className="border-gray-300"
+                  value={formData.cnpj}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="corporateEmail" className='responsive-label'>E-mail Corporativo</Label>
+                <Input
+                  id="corporateEmail"
+                  type="email"
+                  placeholder="Digite o e-mail corporativo"
+                  className="border-gray-300"
+                  value={formData.corporateEmail}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="sector" className='responsive-label'>Setor de Atuação</Label>
+                <Select value={formData.sector} onValueChange={handleSelectChange} required>
+                  <SelectTrigger id="sector">
+                    <SelectValue placeholder="Selecione o setor de atuação" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="admin">Administração</SelectItem>
+                    <SelectItem value="finance">Finanças</SelectItem>
+                    <SelectItem value="law">Direito</SelectItem>
+                    <SelectItem value="health">Saúde</SelectItem>
+                    <SelectItem value="psychology">Psicologia</SelectItem>
+                    <SelectItem value="other">Outro</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+            {/* barra central */}
+            <div className="hidden md:block">
+              <div className="border-l border-gray-300 h-full"></div>
+            </div>
+            {/* lado direito */}
+            <div className="space-y-6 flex-1">
+              <div className="space-y-2">
+                <Label htmlFor="representativeName" className='responsive-label'>Nome do Representante</Label>
+                <Input
+                  id="representativeName"
+                  placeholder="Nome completo do representante"
+                  className="border-gray-300"
+                  value={formData.representativeName}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="cpf" className='responsive-label'>CPF</Label>
+                <Input
+                  id="cpf"
+                  placeholder="Digite o CPF"
+                  className="border-gray-300"
+                  value={formData.cpf}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="email" className='responsive-label'>E-mail</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="Digite o e-mail"
+                  className="border-gray-300"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="phone" className='responsive-label'>Telefone</Label>
+                <Input
+                  id="phone"
+                  placeholder="Digite o telefone"
+                  className="border-gray-300"
+                  value={formData.phone}
+                  onChange={handleInputChange}
+                  required
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-end">
+          <Button
+            type="button"
+            onClick={handleNext}
+            disabled={!isStepOneComplete()}
+            className="bg-purple-500 hover:bg-purple-600 disabled:bg-gray-300"
+          >
+            Continuar
+          </Button>
+        </CardFooter>
+      </Card>
     </Pagina>
   )
 }
--- ghast/src/components/template/Header.tsx
+++ ghast/src/components/template/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className=" fixed flex justify-center px-4 py-2 border-b bg-white dark:dc-main w-full">
+    <header className=" hidden fixed justify-center px-4 py-2 border-b bg-white dark:dc-main w-full md:flex">
         <div className="text-xl font-bold font-audiowide">GHAST CONSULTORIA</div>
     </header>
   );
